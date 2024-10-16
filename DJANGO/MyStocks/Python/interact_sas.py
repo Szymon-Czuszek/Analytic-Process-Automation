c551862da55985417e0111a79e10246cfe12335a
Szymon Czuszek <szymon@czuszek.pl>
2024-10-16 19:10:32 +0200

    Update interact_sas.py
    Updated the script to take files with the keyword prefix.

--- DJANGO/MyStocks/Python/interact_sas.py
+++ DJANGO/MyStocks/Python/interact_sas.py
@@ -8,11 +8,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 import keyring
 import pyautogui
 import time
-import os
-import pyperclip
-import platform
 
-def run_sas_script(user_email, filenames):
+def run_sas_script(prefix, user_email, filenames):
     """
     Automates the process of running a SAS script by interacting with the SAS OnDemand website using Selenium.
     
@@ -25,8 +22,10 @@ def run_sas_script(user_email, filenames):
     start_time = time.time()
     driver = webdriver.Chrome()
     
-    print(filenames)
+    delay = 3
     
+    time.sleep(delay)
+
     try:
         driver.get("https://welcome.oda.sas.com/?event=logout&eventSource=eu-west-1a")
         driver.maximize_window()
@@ -56,13 +55,15 @@ def run_sas_script(user_email, filenames):
         )
         sign_in_button.click()
 
-        time.sleep(2)
-
+        time.sleep(delay)
+        
         # Handle multiple windows
         current_window = driver.current_window_handle
         deploy_button = WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, "//span[contains(@class, 'deploy')]"))
         )
+        
+            
         deploy_button.click()
 
         WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
@@ -71,95 +72,91 @@ def run_sas_script(user_email, filenames):
                 driver.switch_to.window(window_handle)
                 break
 
-        time.sleep(10)
-
+        time.sleep(delay*2)
+            
         # Interaction with files
         files_home_element = WebDriverWait(driver, 60).until(
-            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Files (Home)')]"))
-        )
+            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Files (Home)')]")))
+                
         actions = ActionChains(driver)
         actions.double_click(files_home_element).perform()
 
-        time.sleep(5)
+        time.sleep(delay)
 
         files_stock_element = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Stock Data')]"))
-        )
+            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Stock Data')]")))
+                
         actions.double_click(files_stock_element).perform()
+            
+        time.sleep(delay)
+            
+        files_stock_element = WebDriverWait(driver, 30).until(
+            EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Stock Data')]")))
+            
+        files_stock_element.click()
+            
+        files_upload_element = WebDriverWait(driver, 60).until(
+            EC.visibility_of_element_located((By.XPATH, "//span[contains(@class, 'sasUploadIcon')]")))
+
+        files_upload_element.click()
+            
+        time.sleep(delay)
+
+        actions.send_keys(Keys.TAB).perform()
+
+        time.sleep(delay)
+            
+        actions.send_keys(Keys.ENTER).perform()
+
+        time.sleep(delay)
+        pyautogui.press('tab')
+        time.sleep(delay)
+        # Use hotkey to paste the clipboard contents
+        pyautogui.typewrite(prefix)
+        #pyautogui.hotkey(('command' if platform.system() == 'Darwin' else 'ctrl'), 'v')
+        time.sleep(delay)
+        pyautogui.press('tab')
+        time.sleep(delay)
+        pyautogui.press('tab')
+        time.sleep(delay)
+        pyautogui.press('down')
+        time.sleep(delay)
+
+        if len(filenames) > 1:
+
+            # Hold down the Shift key
+            pyautogui.keyDown('shift')
         
+            for filename in filenames:
+                if filename != filenames[0]:
+                    pyautogui.press('down')
+                    time.sleep(delay)
+            
+            # Release the Shift key
+            pyautogui.keyUp('shift')
         
-        for filename in filenames:
-            
-            # Copy the filename to the clipboard
-            pyperclip.copy(filename)
-            
-            time.sleep(5)
-            
-            # Refresh the page
-            driver.refresh()
-            
-            time.sleep(5)
-            
-            files_stock_element = WebDriverWait(driver, 30).until(
-                EC.visibility_of_element_located((By.XPATH, "//span[contains(text(), 'Stock Data')]"))
-            )
-            
-            files_stock_element.click()
-            
-            files_upload_element = WebDriverWait(driver, 60).until(
-                EC.visibility_of_element_located((By.XPATH, "//span[contains(@class, 'sasUploadIcon')]"))
-            )
+        pyautogui.press('enter')
+        time.sleep(delay)
 
-            files_upload_element.click()
+        # Execute the script
+        actions.send_keys(Keys.ENTER).perform()
+        time.sleep(delay)
             
-            time.sleep(5)
-
-            actions.send_keys(Keys.TAB).perform()
-
-            time.sleep(5)
-            
-            actions.send_keys(Keys.ENTER).perform()
-
-            time.sleep(5)
-            pyautogui.press('tab')
-            time.sleep(5)
-            # Copy the filename to the clipboard
-            pyperclip.copy(filename)
-            time.sleep(5)
-            pyperclip.copy(filename)
-            # Use hotkey to paste the clipboard contents
-            pyautogui.hotkey(('command' if platform.system() == 'Darwin' else 'ctrl'), 'v')
-            # pyautogui.typewrite(filename)
-            time.sleep(5)
-            pyautogui.press('tab')
-            time.sleep(5)
-            pyautogui.press('tab')
-            time.sleep(5)
-            pyautogui.press('down')
-            time.sleep(5)
-            pyautogui.press('enter')
-            time.sleep(5)
-
-            # Execute the script
-            actions.send_keys(Keys.ENTER).perform()
-            time.sleep(5)
-            
-            # Try to click the "Replace" button if the file already exists
-            try:
-                replace_button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//span[@id='decisionDialog_decision1Button_label' and text()='Replace']"))
-                )
-                replace_button.click()
-                print(f"File '{filename}' already exists, clicked 'Replace'.")
-            except Exception as e:
-                print(f"File '{filename}' does not already exist, or 'Replace' button not found.")
+        # Try to click the "Replace" button if the file already exists
+        try:
+            replace_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//span[@id='decisionDialog_decision1Button_label' and text()='Replace']")))
+            replace_button.click()
+            print(f"1 or more file(s) already exists, clicked 'Replace'.")
+        except Exception as e:
+            print(f"None of the files exist, or 'Replace' button not found.")
 
         format_button = WebDriverWait(driver, 60).until(
             EC.visibility_of_element_located((By.XPATH, "//span[@aria-labelledby='perspectiveTabContainer_tabsBC_tab0_formatCodeBtn_label']"))
         )
         format_button.click()
 
-        time.sleep(5)
+        time.sleep(delay)
 
         run_button = WebDriverWait(driver, 60).until(
             EC.visibility_of_element_located((By.XPATH, "//span[@aria-labelledby='perspectiveTabContainer_tabsBC_tab0_submitBtn_label']"))
@@ -177,11 +174,12 @@ def run_sas_script(user_email, filenames):
         print("WebDriver closed")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run a SAS script using Selenium.')
-    parser.add_argument('email', help='Your SAS email.')
-    parser.add_argument('filenames', nargs='+', help='Names of the files to work with (space-separated for multiple files).')
+    parser = argparse.ArgumentParser(description = 'Run a SAS script using Selenium.')
+    parser.add_argument('prefix', help = "Prefix used to filter out the files needed to be uploaded.")
+    parser.add_argument('email', help = 'Your SAS email.')
+    parser.add_argument('filenames', nargs = '+', help = 'Names of the files to work with (space-separated for multiple files).')
 
     args = parser.parse_args()
 
     # Pass the list of files directly to the script
-    run_sas_script(args.email, args.filenames)
+    run_sas_script(args.prefix, args.email, args.filenames)
